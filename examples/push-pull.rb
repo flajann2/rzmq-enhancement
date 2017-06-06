9a1fbbaca3da7f5133fbed75f95603e42eec155a
Fred Mitchell SB <frederick.mitchell@sensorberg.com>
2017-06-06 18:17:45 +0200

    frist release

--- examples/push-pull.rb
+++ examples/push-pull.rb
@@ -1,10 +1,30 @@
 require 'rzmq-enhancement'
+require 'pp'
+require 'thread'
 
 include ZeroMQ
+EP = 'ipc://pushpull.ipc' 
+thr = []
 
 # push
-zeromq :push_example do |ctx|
-  zq_push do
-    
+thr << Thread.new { 
+  (0..10).each do |i|
+    zeromq_push :push_example, EP do |ctx|
+      unless i == 10
+        [i, 'mississippi']
+      else
+        :end_of_stream
+      end
+    end
   end
-end
+}
+
+# pull
+thr << Thread.new { 
+  zeromq_pull_server :pull_example, EP do |payload|
+    pp payload
+    exit if payload == 'end_of_stream'
+  end
+}
+
+thr.each {|t| t.join }
--- lib/rzmq-enhancement.rb
+++ lib/rzmq-enhancement.rb
@@ -2,26 +2,61 @@
 
 require 'ffi-rzmq'
 require 'awesome_print'
+require 'ostruct'
+require 'json'
 
 Thread.abort_on_exception = true
 
 module ZeroMQ
- 
-  def zeromq name, endpoint = 'tcp://127.0.0.1:2200', &block
-    @ctx = ZMQ::Context.create(1)
-    @push_sock = @ctx.socket(ZMQ::PUSH)
-    error_check(@push_sock.setsockopt(ZMQ::LINGER, 0))
-    @rc = @push_sock.bind(endpoint)
-    error_check(@rc)
-    instance_eval &block
-  end
 
-  def zq_push &block
-    ap @ctx
-    instance_eval &block
+ 
+  def zeromq_push name, endpoint = "ipc://#{name}.ipc", &block
+    init_sys
+    h = if @ctxh[name].nil?
+          h = (@ctxh[name] ||= OpenStruct.new)
+          h.ctx = ZMQ::Context.create(1)
+          h.push_sock = h.ctx.socket(ZMQ::PUSH)
+          error_check(h.push_sock.setsockopt(ZMQ::LINGER, 0))
+          rc = h.push_sock.bind(endpoint)
+          error_check(rc)
+          h
+        else
+          @ctxh[name]
+        end
+    
+    if block_given?
+      payload = block.(h.ctx)
+      rc = h.push_sock.send_string(JSON.generate(payload))
+      error_check(rc)
+    end
+    h
+  end  
+
+  # this does an endless loop as a "server"  
+  def zeromq_pull_server name, endpoint = "ipc://#{name}.ipc", &block
+    init_sys
+    h = (@ctxh[name] ||= OpenStruct.new)
+    h.ctx = ZMQ::Context.create(1)
+
+    h.pull_sock = h.ctx.socket(ZMQ::PULL)
+    error_check(h.pull_sock.setsockopt(ZMQ::LINGER, 0))
+    
+    rc = h.pull_sock.connect(endpoint)
+    error_check(rc)
+
+    loop do
+      rc = h.pull_sock.recv_string payload = ''
+
+      block.(JSON.parse(payload))
+    end if block_given?
+    h
   end
   
   private
+  def init_sys
+    @ctxh ||= {}
+  end
+  
   def error_check rc
     if ZMQ::Util.resultcode_ok?(rc)
       false
